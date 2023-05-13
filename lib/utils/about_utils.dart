16f0904e435825a351318ead60ac169f2c085063
Ajaykumar-B-code <bonagiriajaykumar1@gmail.com>
2023-05-13 19:35:54 +0530

    initial commit

---
+++ lib/utils/about_utils.dart
@@ -0,0 +1,7 @@
+class AboutUtils {
+  static const String aboutMeHeadline =
+      'I\'m B Ajay kumar, a Flutter developer and UI designer.';
+
+  static const String aboutMeDetail =
+      'I\'m a Fresh Graduate with Computer Science as Majors from SRM University, Chennai. I have been developing mobile apps for over 2 years now. I have worked in teams for various startups and helped them in launching their prototypes and got valuable learning experience.';
+}
---
+++ lib/utils/project_utils.dart
@@ -0,0 +1,27 @@
+class ProjectUtils {
+  static const List<String> banners = [
+    "assets/projects/snackbar.png",
+    "assets/projects/quranB.png",
+  ];
+
+  static const List<String> icons = [
+    "assets/projects/flutter.png",
+   "assets/projects/flutter.png",
+
+  ];
+
+  static const List<String> titles = [
+    "To-Do Application",
+    "Quotes App",
+  ];
+
+  static const List<String> description = [
+    "It's a list of tasks you need to complete or things what you want to do,most typically in the order of priority",
+    "it generates a random Quotes by using a API",
+  ];
+
+  static const List<String> links = [
+    "https://github.com/Ajaykumar-B-code/Todo",
+    "https://github.com/Ajaykumar-B-code/QuotesApp",
+  ];
+}
